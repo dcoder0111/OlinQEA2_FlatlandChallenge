16d21fe3a452cca6f16492ad7278d9551f497b14
kennethxiong23 <54566745+kennethxiong23@users.noreply.github.com>
2024-03-10 21:30:08 -0400

    added phyisical neato code

--- gradient_ascent.m
+++ gradient_ascent.m
@@ -9,7 +9,7 @@ function gradient_ascent(use_sim, position, heading)
     if use_sim
         neatov2.connect();
     else
-        neatov2.connect('YOUR_NEATO_IP_HERE'); % For real Neato, specify IP
+        neatov2.connect('192.168.16.58'); % For real Neato, specify IP
     end
 
     % Define the landscape grid
@@ -28,9 +28,9 @@ function gradient_ascent(use_sim, position, heading)
 
     step_size = 0.01; % Step size, smaller step size leads to more precise ascent
     threshold = 0.1; % Gradient magnitude threshold for stopping  
+    zvals = double(subs(f_sym, {x, y}, {xvals, yvals}));
 
     if use_sim
-        zvals = double(subs(f_sym, {x, y}, {xvals, yvals}));
         neatov2.setPositionAndOrientation(position(1), position(2), atan2(heading(2), heading(1)));
         neatov2.setFlatlandContours(xvals, yvals, zvals);
         f = figure;
@@ -38,6 +38,7 @@ function gradient_ascent(use_sim, position, heading)
     end
     
     points = [];
+    heading = [];
     % Steepest ascent loop
     while true
         grad_x = grad_f_x(position(1), position(2));
@@ -51,17 +52,29 @@ function gradient_ascent(use_sim, position, heading)
         direction = grad / norm(grad); % Direction of steepest ascent
         position = position + step_size * direction; % Move in that direction
         points(size(points, 1) + 1, :) = position;
+        heading(size(heading, 1) + 1, :) = atan2(direction(2), direction(1));
 
         if use_sim
             neatov2.setPositionAndOrientation(position(1), position(2), atan2(direction(2), direction(1)));
             neatov2.plotSim(); % Update simulation plot
+        else
+
         end
         pause(0.05); % Slow down the loop for visualization
     end
-    [xvals, yvals] = meshgrid(linspace(-1.25, 1.25, 100), linspace(-0.5, 0.5, 100));
-    syms x y
-    f_sym = 8*exp(-1.5*(x + y - 0.75).^2 - 0.5*(x - y - 0.75).^2) + 6*exp(-(x + 0.75).^2 - (y + 0.25).^2);
-    zvals = double(subs(f_sym, {x, y}, {xvals, yvals}));
+    %Linear velocity is constant due to step size
+    linear_velocity = diff(points);
+    linear_velocity = sqrt(linear_velocity(:,1).^2 + linear_velocity(:,2).^2);
+    angular_velocity = diff(heading); %find omega from the changing heading
+    beta = 3; %velocity scale factor b/c it's slow
+    v_l = (linear_velocity - angular_velocity * 0.248/2) .* beta;
+    v_r = (linear_velocity + angular_velocity * 0.248/2) .* beta;
+    if ~use_sim
+        for i=1:size(v_l)
+            neatov2.setVelocities(v_l(i), v_r(i))
+            pause(0.4) %b/c of the lack of sample sizes run each one for longer
+        end
+    end
     %theoretical trajectory plot
     f = figure;
     contour(xvals, yvals, zvals, 20);
